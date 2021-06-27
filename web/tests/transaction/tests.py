21373b81dcc04708744607fff36c9938b3b12971
Valeriy Shagur <vshagur@gmail.com>
2021-06-27 12:08:51 +0700

    [tests] add tests for transaction endpoint

--- web/tests/transaction/tests.py
+++ web/tests/transaction/tests.py
@@ -44,3 +44,41 @@ def test_auth_client_can_get_transactions_list(auth_client):
     resp = auth_client.get(reverse('transaction-list'))
     assert resp.status_code == status.HTTP_200_OK
     assert len(resp.json()) == 10
+
+
+@pytest.mark.django_db
+class TestCreateTransaction:
+
+    @classmethod
+    def setup_class(cls):
+        cls.wallet = Wallet.objects.first()
+        cls.transactions = Transaction.objects.all()
+        cls.count = cls.transactions.count()
+        cls.url = reverse('transaction-list')
+        cls.data = {
+            'wallet': cls.wallet.id,
+            'comment': 'some comment',
+            'balance': '42.42',
+        }
+
+    def test_anon_client_can_not_create_transaction(self, anon_client):
+        resp = anon_client.post(self.url, data=self.data)
+        assert resp.status_code == status.HTTP_401_UNAUTHORIZED
+        assert Transaction.objects.all().count() == self.count
+
+    @pytest.mark.django_db
+    def test_auth_client_can_create_transaction(self, auth_client):
+        resp = auth_client.post(self.url, data=self.data)
+        assert resp.status_code == status.HTTP_201_CREATED
+        assert Transaction.objects.all().count() == self.count + 1
+        resp_data = resp.json()
+        assert 'id' in resp_data
+        pk = resp_data.get('id')
+        assert Transaction.objects.filter(id=pk).exists()
+        assert resp_data.get('comment') == self.data.get('comment')
+        assert resp_data.get('wallet') == self.wallet.id
+        assert Decimal(str(resp_data.get('balance'))) == Decimal(self.data.get('balance'))
+        transaction = Transaction.objects.get(id=pk)
+        assert transaction.comment == self.data.get('comment')
+        assert transaction.wallet == self.wallet
+        assert transaction.balance == Decimal(self.data.get('balance'))
